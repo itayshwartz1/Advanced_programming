6eec9730a2d75bfa76dc0848d2cdb8e47a526eb4
roeige <roy508@madaimas.org.il>
2021-10-13 11:02:08 +0300

    test ignored to note 1

---
+++ Test_Original.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yhood on 13/10/2021.
+//
+
+#include "Test_Original.h"
---
+++ Test_Original.h
@@ -0,0 +1,14 @@
+//
+// Created by yhood on 13/10/2021.
+//
+
+#ifndef DEV_TEST_ORIGINAL_H
+#define DEV_TEST_ORIGINAL_H
+
+
+class Test_Original {
+
+};
+
+
+#endif //DEV_TEST_ORIGINAL_H
