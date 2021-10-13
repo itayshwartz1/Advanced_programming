a92e1bf0646f0898b016eaba199b659ba4a3e129
itayshwartz1 <itay2007@gmail.com>
2021-10-13 11:19:19 +0300

    s

--- Test_Original.cpp
+++ Test_Original.cpp
@@ -2,8 +2,28 @@
 // Created by yhood on 13/10/2021.
 //
 
+#include <cmath>
 #include "Test_Original.h"
+// Function for calculating variance
+float internetVariance(int a[], int n) {
+    // Compute mean (average of elements)
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+        sum += a[i];
+    double mean = (double)sum /
+                  (double)n;
+
+    // Compute sum squared
+    // differences with mean.
+    double sqDiff = 0;
+    for (int i = 0; i < n; i++)
+        sqDiff += (a[i] - mean) *
+                  (a[i] - mean);
+    return sqDiff / n;
+}
+
 void testVar(float** bArr){
+
 
 }
 int main(){
@@ -14,4 +34,5 @@ int main(){
     float *e = new float[8]{562, 23, 20, 765, 323, 36, 2, 9};
     float *f = new float[9]{3, 216, 16, 62, 20, 4, 36, 6, 45};
     float *g = new float[10]{89, 416, 214, 6, 56, 56, 417, 52, 9, 10};
+    float** bArr = new float[7][a,b,c,d,e,f,g,]
 }
