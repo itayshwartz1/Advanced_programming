02bfd53983bb4d357fee19479655180b07c7683f
roeige <roy508@madaimas.org.il>
2021-10-13 11:14:51 +0300

    test ignored to note 2

--- Test_Original.cpp
+++ Test_Original.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "Test_Original.h"
+void testVar(float** bArr){
+
+}
+int main(){
+    float *a = new float[4]{1, 2, 3, 4};
+    float *b = new float[5]{55, 885, 52, 529, 7};
+    float *c = new float[6]{7, 65, 32, 0, 23, 41};
+    float *d = new float[7]{8, 26, 1, 9, 9, 44, 42};
+    float *e = new float[8]{562, 23, 20, 765, 323, 36, 2, 9};
+    float *f = new float[9]{3, 216, 16, 62, 20, 4, 36, 6, 45};
+    float *g = new float[10]{89, 416, 214, 6, 56, 56, 417, 52, 9, 10};
+}
