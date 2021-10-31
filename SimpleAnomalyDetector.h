187eb4a5d77cf008598654a94d00a1518651839a
roeige <roy508@madaimas.org.il>
2021-10-31 20:39:14 +0200

    added some classes and headers-- SImpleAnomalyDetector

--- SimpleAnomalyDetector.h
+++ SimpleAnomalyDetector.h
@@ -5,18 +5,31 @@
 #ifndef DEV_SIMPLEANOMALYDETECTOR_H
 #define DEV_SIMPLEANOMALYDETECTOR_H
 
-struct correlatedFeatures{
-    string feature1,feature2; // names of the correlated features
+#include "AnomalyDetector.h"
+#include "anomaly_detection_util.h"
+
+struct correlatedFeatures {
+    string feature1, feature2; // names of the correlated features
     float corrlation;
     Line lin_reg;
     float threshold;
 };
-class SimpleAnomalyDetector:public TimeSeriesAnomalyDetector{
+
+class SimpleAnomalyDetector : public TimeSeriesAnomalyDetector {
 public:
+    correlatedFeatures c;
+
+    explicit SimpleAnomalyDetector(correlatedFeatures c);
+
     SimpleAnomalyDetector();
-    virtual ~SimpleAnomalyDetector();
-    virtual void learnNormal(const TimeSeries& ts);
-    virtual vector<AnomalyReport> detect(const TimeSeries& ts);
-    vector<correlatedFeatures> getNormalModel();
+
+    ~SimpleAnomalyDetector() override;
+
+    virtual void learnNormal(const TimeSeries &ts) override;
+
+    virtual vector <AnomalyReport> detect(const TimeSeries &ts);
+
+    virtual vector <correlatedFeatures> getNormalModel();
 };
+
 #endif //DEV_SIMPLEANOMALYDETECTOR_H
