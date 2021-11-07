2f0f64ebf54a6a73a39fd9203bbf6aa96c6ad326
roeige <roy508@madaimas.org.il>
2021-11-07 21:21:08 +0200

    fixed bugg

--- anomaly_detection_util.cpp
+++ anomaly_detection_util.cpp
@@ -112,8 +112,8 @@ Line linear_reg(Point **points, int size) {
     float *xArr = new float[size];
     float *yArr = new float[size];
     for (int i = 0; i < size; i++) {
-        xArr[i] = points[i].x;
-        yArr[i] = points[i].y;
+        xArr[i] = points[i]->x;
+        yArr[i] = points[i]->y;
     }
     float covariance = cov(xArr, yArr, size);
     float variance = var(xArr, size);
